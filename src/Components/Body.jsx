884ff93155784593a8f7d4ba3c67d84b9524513f
mahesh2291 <gmahesh252525@gmail.com>
2025-09-02 11:43:41 -0400

    infinite scroll

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -8,26 +8,38 @@ function Body() {
   
   useEffect(()=>{
       fetchMemes()
+
+      window.addEventListener("scroll",handleScroll)
+
+      return ()=>window.removeEventListener("scroll",handleScroll)
   },[])
+
+  const handleScroll=()=>{
+     if(window.scrollY+window.innerHeight>=document.body.scrollHeight) {
+      console.log('bottom')
+        fetchMemes()
+     }
+  }
 
   const fetchMemes=async()=>{
     setIsloading(true)
      const memesData=await fetch('https://meme-api.com/gimme/20')
      const memesDataJson=await memesData.json()
-     setMemes(memesDataJson.memes)
-    setTimeout(()=>{
-       setIsloading(false)
-    },3000)
+     setMemes((oldMemes)=>[...oldMemes,...memesDataJson.memes])
+    setIsloading(false)
+    
   }
 
   return (
     <>
     <div style={{display:'flex',flexWrap:'wrap',alignItems:'center'}}>
-      {isLoading? <Shimmer /> : memes?.map((meme)=>{
+      {memes?.map((meme,i)=>{
+        console.log(memes)
         return (
           <Card key={meme.id} author={meme.author} image={meme.url} title={meme.title} />
         )
       })}
+      {isLoading && <Shimmer />}
     </div>
     </>
   )
--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Shimmer=()=>{
     return Array(20).fill(0).map((n,i)=>(
-<div style={{border:'3px solid black',padding:'10px 10px',borderRadius:'10px',width:'500px',height:'500px',margin:'50px 50px'}}>
+<div key={i} style={{border:'3px solid black',padding:'10px 10px',borderRadius:'10px',width:'500px',height:'500px',margin:'50px 50px'}}>
             
-            <div key={i} style={{margin:'20px 80px',background:'gray',height:'350px',width:'350px'}}>
+            <div  style={{margin:'20px 80px',background:'gray',height:'350px',width:'350px'}}>
 
             </div>
             <div style={{background:'gray'}} ><h3 style={{visibility:'hidden'}}>s</h3></div>
