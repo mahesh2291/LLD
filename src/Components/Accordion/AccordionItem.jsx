30747c8e122c08cce0abbddc26ef1a2e37b97b16
mahesh2291 <gmahesh252525@gmail.com>
2025-09-03 11:53:51 -0400

    cursor pointer

--- src/Components/Accordion/AccordionItem.jsx
+++ src/Components/Accordion/AccordionItem.jsx
@@ -3,7 +3,7 @@
 const AccordionItem=({title,description,isOpenAccordion,setIsOpen})=>{
    return (
     <div style={{marginTop:'10px'}}>
-     <div onClick={()=>setIsOpen()} style={{border:'1px solid black',padding:"10px 10px",display:'flex',justifyContent:'space-between', background:'grey'}}>{title}<span>{isOpenAccordion? '↑' : '↓'}</span></div>
+     <div onClick={()=>setIsOpen()} style={{border:'1px solid black',padding:"10px 10px",display:'flex',justifyContent:'space-between', background:'grey',cursor:'pointer'}}>{title}<span>{isOpenAccordion? '↑' : '↓'}</span></div>
     {
       isOpenAccordion && <div style={{border:'1px solid black',padding:"10px 10px"}}>{description}</div>
     } 
